827680170ef35f0e7c8e7cfd2a8c2289afcc565a
Opeyemi-Omotayo <Opeyemi.Motayo1@gmail.com>
2023-07-16 08:54:02 +0100

    created the header and banner

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,245 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+<header class="header-area">
+            <div class="container">
+               <div class="row d_flex">
+                  <div class=" col-1 ">
+                     <div class="logo">
+                        <router-link to="/">Pet Finder</router-link>
+                     </div>
+                  </div>
+                  <div class="col-2">
+                     <div class="navbar-area">
+                        <nav class="site-navbar">
+                           <ul>
+                              <li><router-link to="/">Pet Finder</router-link></li>
+                              <li><router-link to="/">About</router-link></li>
+                              <li><router-link to="/">Why Us</router-link></li>
+                              <li><router-link to="/">Contact Us</router-link></li>
+                           </ul>
+                           <button class="nav-toggler">
+                           <span></span>
+                           </button>
+                        </nav>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </header>
   <router-view/>
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Poppins', sans-serif;
+   font-weight: 300;
+  color: #444;
+  line-height: 1.9;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+      background-color: #f3f3f3;
+
 }
 
-nav {
-  padding: 30px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: inherit;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+html {
+  font-size: 100%;
+  box-sizing: border-box;
+}
+.header-area {
+    border-top: #fdc001 solid 40px;
+    width: 100%;
+}
+.col-1{
+   position: relative;
+  width: 100%;
+  min-height: 1px;
+  padding-right: 15px;
+  padding-left: 15px;
+  -ms-flex: 0 0 25%;
+    flex: 0 0 25%;
+    max-width: 25%;
+}
+.col-1{
+ -ms-flex: 0 0 25%;
+    flex: 0 0 25%;
+    max-width: 25%;
+}
+.col-2{
+  text-align: right;
+  align-items: right;
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
+.logo a {
+    color: #363636;
+    font-size: 38px;
+    font-weight: 400;
+    text-decoration: none;
 }
+
+.site-navbar {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.site-navbar ul {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    display: flex;
+}
+
+.site-navbar ul li a {
+    color: #363636;
+    font-size: 17px;
+    display: block;
+    text-decoration: none;
+    text-transform: uppercase;
+}
+
+.site-navbar ul li {
+    padding-right: 70px;
+}
+
+.site-navbar ul li:last-child {
+    padding-right: 0;
+}
+
+.site-navbar ul li a:hover {
+    color: #fdc001;
+}
+.site-navbar ul li a.router-link-exact-active {
+    color: #fdc001;
+}
+ .nav-toggler {
+    border: 3px solid #363636;
+    padding: 5px;
+    background-color: transparent;
+    cursor: pointer;
+    height: 39px;
+    display: none;
+    z-index: 99999;
+}
+
+.nav-toggler span,
+.nav-toggler span:before,
+.nav-toggler span:after {
+    width: 28px;
+    height: 3px;
+    background-color: #363636;
+    display: block;
+    transition: .3s;
+}
+
+.nav-toggler span:before {
+    content: '';
+    transform: translateY(-9px);
+}
+
+.nav-toggler span:after {
+    content: '';
+    transform: translateY(6px);
+}
+@media (min-width: 992px) and (max-width: 1199px) {
+    .site-navbar ul li {
+        padding-right: 57px;
+    }
+}
+@media (min-width: 768px) and (max-width: 991px) {
+    .site-navbar ul li {
+        padding-right: 31px;
+    }
+}
+@media (min-width: 576px) and (max-width: 767px) {
+    /* navbar css for mobile start */
+     .nav-toggler {
+        display: block;
+        position: absolute;
+        right: 15px;
+        z-index: 99999;
+    }
+    .site-navbar {
+        min-height: 60px;
+    }
+    .site-navbar ul {
+        position: absolute;
+        width: 100%;
+        left: -15px;
+        top: 60px;
+        flex-direction: column;
+        align-items: center;
+        background-color: #fff;
+        max-height: 0;
+        overflow: hidden;
+        transition: .3s;
+        z-index: 99999;
+        box-shadow: 0 0 10px rgb(192, 190, 182);
+    }
+    .site-navbar ul li {
+        width: 100%;
+        text-align: left;
+        padding-right: 0;
+    }
+    .site-navbar ul li a {
+        padding: 25px 25px 0px 25px;
+    }
+    .site-navbar ul.open {
+        max-height: 100vh;
+        padding-bottom: 25px;
+        overflow: visible;
+    }
+}
+
+@media (max-width: 575px) {
+    .logo {
+        text-align: left;
+    }
+
+    /* navbar css for mobile start */
+    .nav-toggler {
+        display: block;
+        position: absolute;
+        right: 15px;
+        z-index: 99999;
+    }
+    .navbar-area {
+        margin-top: -67px;
+    }
+    .site-navbar {
+        min-height: 60px;
+    }
+    .site-navbar ul {
+        position: absolute;
+        width: 100%;
+        left: 0px;
+        top: 0px;
+        flex-direction: column;
+        align-items: center;
+        background-color: #fff;
+        max-height: 0;
+        overflow: hidden;
+        transition: .3s;
+        z-index: 99999;
+        box-shadow: 0 0 10px rgb(192, 190, 182);
+    }
+    .site-navbar ul li {
+        width: 100%;
+        text-align: left;
+        padding-right: 0;
+    }
+    .site-navbar ul li a {
+        padding: 25px 25px 0px 25px;
+    }
+    .site-navbar ul.open {
+        max-height: 100vh;
+        padding-bottom: 25px;
+        overflow: visible;
+    }
+}
+
 </style>
---
+++ src/components/Banner.vue
@@ -0,0 +1,215 @@
+<template>
+  <div class="container">
+    <div class="carousel-caption relative">
+      <div class="row d_flex">
+        <div class="col-1">
+          <div class="creative">
+            <h1>Find <br />A Dog</h1>
+            <p>
+        Are you ready to experience the joy of dog adoption? At Pet Finder, we believe that every dog deserves a loving and forever home. Our mission is to connect compassionate individuals like you with adorable dogs in need, creating lifelong bonds filled with love and companionship.            </p>
+            <button class="read_more">Contact us</button>
+          </div>
+        </div>
+        <div class="col-2">
+          <div class="row mar_right">
+            <div class="sub">
+              <div class="agency">
+                <figure><img src="https://images.dog.ceo/breeds/spaniel-cocker/n02102318_10019.jpg" alt="#" /></figure>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Banner"
+}
+</script>
+
+<style>
+.container {
+    min-width: 992px ;
+    margin-left: 5rem;
+  }
+  .carousel-caption {
+  position: absolute;
+  right: 15%;
+  bottom: 20px;
+  left: 15%;
+  z-index: 10;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  color: #fff;
+  text-align: center;
+}
+.relative {
+    position: inherit;
+    left: 0;
+    right: 0;
+    text-align: center;
+    position: inherit;
+}
+.row {
+  display: -ms-flexbox;
+  display: flex;
+  -ms-flex-wrap: wrap;
+  flex-wrap: wrap;
+  margin-right: -15px;
+  margin-left: -15px;
+}
+.d_flex {
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+}
+.col-1, .sub, .col-2{
+  position: relative;
+  width: 100%;
+  min-height: 1px;
+  padding-right: 15px;
+  padding-left: 15px;
+}
+.creative {
+    text-align: left;
+}
+
+.creative h1 {
+    font-size: 80px;
+    line-height: 90px;
+    color: #101010;
+    font-weight: bold;
+}
+
+.creative p {
+    color: #101010;
+    padding: 30px 0 55px 0;
+    line-height: 30px;
+}
+
+.creative .read_more {
+    display: inline-block;
+    margin-right: 5px;
+}
+ .col-1 {
+    -ms-flex: 0 0 41.666667%;
+    flex: 0 0 41.666667%;
+    max-width: 41.666667%;
+  }
+  .sub {
+    -ms-flex: 0 0 50%;
+    flex: 0 0 50%;
+    max-width: 50%;
+  }
+  .col-2 {
+    -ms-flex: 0 0 58.333333%;
+    flex: 0 0 58.333333%;
+    max-width: 58.333333%;
+  }
+  .mar_right {
+    margin-right: -90px;
+}
+.agency figure {
+    margin: 0;
+}
+
+.agency figure img {
+    filter: drop-shadow(4px 6px 5px #0d03034f);
+    border-top-right-radius:  100px;
+    border-bottom-left-radius:  100px;
+    margin-left: 3rem;
+}
+.read_more {
+    font-size: 16px;
+    background: #363636;
+    max-width: 204px;
+    width: 100%;
+    transition: ease-in all 0.5s;
+    color: #fff;
+    display: block;
+    height: 61px;
+    line-height: 61px;
+    text-align: center;
+    border-radius: 30px;
+    border: #363636;
+    text-transform: uppercase;
+    font-weight: bold;
+}
+
+.read_more:hover {
+    background: #fdc001;
+    color: #fff;
+}
+
+@media (min-width: 992px) and (max-width: 1199px) {
+    .mar_right {
+        margin-right: 0;
+    }
+    .creative .read_more {
+        max-width: 185px;
+        margin-right: 0;
+    }
+    .container {
+    max-width: 960px;
+  }
+}
+
+@media (min-width: 768px) and (max-width: 991px) {
+    .mar_right {
+        margin-right: 0;
+    }
+    .container {
+    max-width: 720px;
+  }
+    .creative h1 {
+        font-size: 56px;
+        line-height: 70px;
+    }
+    .creative p {
+        padding: 5px 0 15px 0;
+    }
+    .creative .read_more {
+        max-width: 142px;
+        margin-right: 0;
+        }
+        }
+
+@media (min-width: 576px) and (max-width: 767px) {
+    .mar_right {
+        margin-right: 0px;
+    }
+    .about_img,
+    .agency {
+        margin-top: 30px;
+    }
+    .agency {
+        margin-top: 30px;
+    }
+    .container {
+    max-width: 540px;
+  }
+   }
+   @media (max-width: 575px) {
+    
+    .about_img,
+    .agency {
+        margin-top: 30px;
+    }
+    .creative .read_more {
+        margin-bottom: 5px;
+        margin-right: 0;
+    }
+    .mar_right {
+        margin-right: 0px;
+    }
+}
+
+@media (min-width: 1200px) {
+  .container {
+    max-width: 1140px;
+  }
+}
+</style>
