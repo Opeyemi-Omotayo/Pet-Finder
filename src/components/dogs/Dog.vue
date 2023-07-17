5c4dd7fbed82d09dd976fd828cc648de28058176
Opeyemi-Omotayo <Opeyemi.Motayo1@gmail.com>
2023-07-17 08:47:11 +0100

    finished filtering

--- src/components/dogs/Dog.vue
+++ src/components/dogs/Dog.vue
@@ -1,24 +1,30 @@
 <template>
    <Search @search-results="updateDogs"/>
-   <div class="card-list dog-filter">
-   <div v-if="filteredDogs.length > 0">
-    <div v-for="dog in filteredDogs" :key="dog.id" class="card">
-      <img v-lazy="dog" :alt="dog.breed" class="card-image" />
-      <h3 class="card-title">{{ dog.breed }}</h3>
-        <p class="card-breed">
+   <div class="container-dog">
+    <div v-if="searched && error">
+      <p>No results found for "{{ searchTerm }}".</p>
+    </div>
+    
+    <div v-else-if="filteredDogs.length > 0" class="card-lists">
+      <div v-for="dog in filteredDogs" :key="dog.id" class="card">
+        <img v-lazy="dog" :alt="dog.breed" class="card-image" />
+      <div class="card-content">
+        <h3 class="dog-title">{{ searchTerm }}</h3>
+        <p class="dog-description">
           Lorem ipsum dolor sit amet consectetur adipisicing elit.
         </p>
+        <router-link :to="{ name: 'DogDetailsview', params: { breed: searchTerm } }">
+          <button class="see_more">See More</button>
+        </router-link>
+      </div>
+      </div>
     </div>
-  </div>
-  <div v-else>
-    <p>No results found.</p>
-  </div>
-     </div>
-
-  <div class="container">
-    <h3 class="avail-h1">Available Pets</h3>
+    <div v-else>
+      <p>You haven't searched for any breed.</p>
+      <h3 class="avail-h1">Available Pets</h3>
     <loading :loading="isLoading" />
     <DogCard :dogs="dogs" />
+    </div>
   </div>
 </template>
 
@@ -35,6 +41,10 @@ export default {
     return {
       isLoading: true,
       filteredDogs: [],
+      searchTerm: '',
+      error: false, 
+      searched: false
+
     };
   },
   computed: {
@@ -47,7 +57,15 @@ export default {
       this.$store.dispatch('fetchDogs');
     },
     updateDogs(data) {
-      this.filteredDogs = data;
+      if (data.error) {
+        this.error = true; 
+        this.filteredDogs = [];
+      } else {
+        this.filteredDogs = data.data;
+        this.searchTerm = data.searchTerm;
+       this.error = false; 
+      }
+      this.searched = true;
     },
   },
   mounted() {
@@ -63,5 +81,52 @@ export default {
 .avail-h1{
   text-align: center;
   margin: 5rem;
+}
+.card-lists {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  gap: 20px;
+  margin-top: 3rem;
+}
+.container-dog {
+  min-width: 992px ;
+    margin-left: 5rem;
+    margin-bottom: 25rem;
+}
+
+.dog-list {
+  display: flex;
+  flex-wrap: wrap;
+  height: 380px;
+
+}
+
+.dog-card {
+  flex: 0 0 300px;
+  margin: 1rem;
+  padding: 1rem;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  background-color: #fff;
+}
+.dog-image {
+  width: 100%;
+  height: 200px;
+  object-fit: cover;
+  border-radius: 8px;
+}
+
+.dog-title {
+  font-size: 18px;
+  font-weight: bold;
+  margin-bottom: 5px;
+  text-transform: uppercase;
+  text-align: center;
+}
+
+.dog-description {
+  color: #777;
+  text-align: center;
 }
 </style>
