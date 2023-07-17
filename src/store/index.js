44668dd619aca30294965f6e3a9af157cfd91855
Opeyemi-Omotayo <Opeyemi.Motayo1@gmail.com>
2023-07-17 15:12:47 +0100

    catching data

--- src/store/index.js
+++ src/store/index.js
@@ -2,38 +2,51 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
-    dogs: [], 
+    dogs: getCachedData() || [], 
   },
-  getters: {
-  },
+  getters: {},
   mutations: {
     SET_DOGS(state, dogs) {
       state.dogs = dogs;
+      cacheData(dogs); 
     },
   },
   actions: {
     fetchDogs({ commit }) {
-      fetch('https://dog.ceo/api/breeds/list/all')
-        .then((response) => response.json())
-        .then((data) => {
-          const breeds = Object.keys(data.message);
-          const fetchImages = breeds.map((breed) =>
-            fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
-              .then((response) => response.json())
-              .then((data) => ({
-                breed,
-                image: data.message,
-              }))
-          );
-          Promise.all(fetchImages).then((dogs) => {
-            commit('SET_DOGS', dogs);
+      const cachedData = getCachedData();
+      if (cachedData) {
+        commit('SET_DOGS', cachedData); 
+      } else {
+        fetch('https://dog.ceo/api/breeds/list/all')
+          .then((response) => response.json())
+          .then((data) => {
+            const breeds = Object.keys(data.message);
+            const fetchImages = breeds.map((breed) =>
+              fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
+                .then((response) => response.json())
+                .then((data) => ({
+                  breed,
+                  image: data.message,
+                }))
+            );
+            Promise.all(fetchImages).then((dogs) => {
+              commit('SET_DOGS', dogs);
+            });
+          })
+          .catch((error) => {
+            console.error('Error fetching dogs:', error);
           });
-        })
-        .catch((error) => {
-          console.error('Error fetching dogs:', error);
-        });
+      }
     },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
+
+function cacheData(data) {
+  localStorage.setItem('dogs', JSON.stringify(data));
+}
+
+function getCachedData() {
+  const cachedData = localStorage.getItem('dogs');
+  return cachedData ? JSON.parse(cachedData) : null;
+}
